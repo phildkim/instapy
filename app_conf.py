413c017efd803a9b3ee1e5d5afe642abb107b334
Philip D. Kim <philipdanielkim@gmail.com>
2020-10-05 01:52:59 -0700

    Delete app_conf.py

--- app_conf.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-from passlib.hash import sha256_crypt
-INSTAPY_USERNAME = 'jasminecreamery'
-INSTAPY_PASSWORD = 'JCream2018'
-INSTAPY_SECRET = sha256_crypt.hash('jasminecreameryJCream2018')
